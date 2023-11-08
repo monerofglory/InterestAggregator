87f7e78d34ae61b2bc65f6e28dbfba76b9a7842b
Alex Hoare <alexander_hoare@hotmail.co.uk>
2023-11-08 11:10:17 +0000

    Re-added FeedStorageFunctionalTests

---
+++ InterestAggregatorFunctionalTests/FeedStorageFunctionalTests.cs
@@ -0,0 +1,69 @@
+﻿using InterestAggregatorFunction.Config;
+using InterestAggregatorFunction.Services.FeedStorage;
+using InterestAggregatorFunction.FeedServices;
+using System.Reflection;
+using Xunit;
+
+namespace InterestAggregatorFunctionalTests
+{
+    public class FeedStorageFunctionalTests
+    {
+        private readonly FeedStorageYaml _sut;
+
+        public FeedStorageFunctionalTests()
+        {
+            IFeedConfig config = new FeedConfig
+            {
+                YamlPath = Assembly.GetExecutingAssembly().Location + "/../../../../../InterestAggregatorFunction/bin/Debug/net7.0/YamlFeedList.yml"
+            };
+            _sut = new(config);
+        }
+
+        [Fact]
+        public void Assert_FirstFeed_IsCK3()
+        {
+            //Act
+            var allFeeds = _sut.GetFeeds();
+
+            //Assert
+            Assert.Equal("Crusader Kings", allFeeds.First().Name);
+        }
+
+        [Fact]
+        public void Assert_FeedsReturn()
+        {
+            //Act
+            var allFeeds = _sut.GetFeeds();
+
+            //Assert
+            Assert.NotNull(allFeeds);
+            Assert.NotEmpty(allFeeds);
+        }
+
+        [Fact]
+        public void Assert_WhenAllFeedsFetched_AtLeastOneReddit()
+        {
+            //Act
+            var allFeeds = _sut.GetFeeds();
+
+            var feeds = allFeeds.Where(x => x.FeedType == FeedTypeEnum.Reddit);
+
+            //Assert
+            Assert.NotNull(feeds);
+            Assert.NotEmpty(feeds);
+        }
+
+        [Fact]
+        public void Assert_WhenAllFeedsFetched_AtLeastOneSteam()
+        {
+            //Act
+            var allFeeds = _sut.GetFeeds();
+
+            var feeds = allFeeds.Where(x => x.FeedType == FeedTypeEnum.Steam);
+
+            //Assert
+            Assert.NotNull(feeds);
+            Assert.NotEmpty(feeds);
+        }
+    }
+}
