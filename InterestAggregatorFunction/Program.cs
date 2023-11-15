ed0c5b57ca8ccb8cdbf3e647b9a111d2d0d0443e
BULLHORN\alex.hoare <alexander.hoare@invenias.com>
2023-11-15 10:12:33 +0000

    Initial dockerisation attempt

--- InterestAggregatorFunction/Program.cs
+++ InterestAggregatorFunction/Program.cs
@@ -4,12 +4,11 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        Console.WriteLine("Starting main");
         var host = new HostBuilder()
             .ConfigureFunctionsWorkerDefaults()
             .Build();
 
         host.Run();
     }
-
-
 }
