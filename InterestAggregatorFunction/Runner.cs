9ba2aaf77b590db038afa8d1577208f24ce0227f
Alex Hoare <alexander_hoare@hotmail.co.uk>
2023-11-19 21:11:17 +0000

    Changed the location of the YAML

--- InterestAggregatorFunction/Runner.cs
+++ InterestAggregatorFunction/Runner.cs
@@ -16,7 +16,7 @@ namespace InterestAggregatorFunction
         }
 
         [Function("Runner")]
-        public void Run([TimerTrigger("0 0 21 * * *")] TimerInfo myTimer)
+        public void Run([TimerTrigger("0 0 22 15 * *")] TimerInfo myTimer)
         {
             //Register dependencies
             ServiceCollection services = new();
